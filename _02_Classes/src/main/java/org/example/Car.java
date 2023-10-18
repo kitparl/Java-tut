aed0e174c7bb56f82d120ac61c0ac051603ef846
Pranshu Singh Bisht <pranshu.java@gmail.com>
2023-10-18 09:56:34 +0530

    day-2 tut

---
+++ _02_Classes/src/main/java/org/example/Car.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Car {
+    //Properties (Fields or Members):
+    int wheel; //property
+    int engine; //property
+    String color; //property
+
+
+    public void speed(){  //behav., method, function
+
+    }
+
+    public void weight(){  //behav., method, function
+
+    }
+}
+
+
+// 1obj (3, 1, black),  2obj (8, 2, pink),
+
+// class is a blueprint or template for a creating an object
+// object is the instance of class
+
+
+// (access modifiers   {keywords:}) -->  Public, Private, Protected
+
+
+//  HW
+//  PascalNamingConvention, Snakecase ??
+
